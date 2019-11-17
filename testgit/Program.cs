a6633ca82732fa0d7bb11f2c5eb51be339799a87
archi-Doc <archidoc422@gmail.com>
2019-11-17 18:37:05 +0900

    1

--- testgit/Program.cs
+++ testgit/Program.cs
@@ -9,15 +9,7 @@ namespace testgit
             Console.WriteLine("Hello World2");
             Console.WriteLine("modified by note-7");
 
-            Console.WriteLine("2");
-            Console.WriteLine("3");
-            Console.WriteLine("4");
-
-            Console.WriteLine("a");
-            Console.WriteLine("b");
-
-            Console.WriteLine("c");
-            Console.WriteLine("d");
+            test();
         }
 
         static void test()
